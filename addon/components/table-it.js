576bcc58b7b5cb848e5e1f9b91913d5af6ef9225
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-04-05 22:16:54 -0400

    Adding pagination

--- addon/components/table-it.js
+++ addon/components/table-it.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'table'
+  totalPages: function() {
+    return this.get('rows').get('content.meta.total_pages');
+  }.property(),
+  actions: {
+    setPage: function(current) {
+      this.sendAction('action', current);
+    }
+  }
 });
