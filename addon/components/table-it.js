9a5d0e0fef1b898ba5e81ae0ac3ad57d5b0dc0cd
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-04-07 15:46:53 -0400

    Adding a loading screen to the table

--- addon/components/table-it.js
+++ addon/components/table-it.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   totalPages: function() {
     return this.get('rows').get('content.meta.total_pages');
-  }.property("rows.content.meta.total_pages"),
+  }.property('rows.content'),
   actions: {
     setPage: function(current) {
       this.sendAction('action', current);
